2a2724497f0504c121527e7ec96d56b05b1288b0
senkentarou <masahiro-senda@c-fo.com>
2022-05-08 13:59:02 +0900

    :recycle: move to blacklist component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
-import {
-  AppBar,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TextField,
-  Typography
-} from '@mui/material';
+import React from 'react';
+import { AppBar, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
+
+import { BlackListTable } from './BlackListTable';
 
 const MyAppBar = styled(AppBar)({
   width: '100%',
@@ -25,64 +15,13 @@ const AppTitle = styled(Typography)({
   marginLeft: '1rem'
 });
 
-type BlackListStorage = { [key: string]: Array<BlackListRow> };
-type BlackListRow = {
-  pattern?: string;
-  regexp?: boolean;
-};
-
 export const Home: React.VFC = () => {
-  const [blackListTable, setBlackListTable] = useState<Array<BlackListRow>>([]);
-  const [blackListRow, setBlackListRow] = useState<BlackListRow>({});
-
-  const handleSetList = () => {
-    if (Object.keys(blackListRow).length === 0) {
-      return;
-    }
-    const nextList = [...blackListTable, blackListRow];
-    chrome.storage.local.set({ blackListTable: nextList }, () => {
-      setBlackListTable(nextList);
-      setBlackListRow({});
-    });
-  };
-
-  useEffect(() => {
-    chrome.storage.local.get('blackListTable', (data: BlackListStorage) => {
-      setBlackListTable(data['blackListTable'] || []);
-    });
-  }, []);
-
   return (
     <>
       <MyAppBar position="static">
         <AppTitle variant="h4">Auto Close Tab</AppTitle>
       </MyAppBar>
-      <Typography variant="h5">Blacklisted sites</Typography>
-      <TableContainer>
-        <Table stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell>pattern</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {blackListTable.map((data, idx) => {
-              return (
-                <TableRow key={idx}>
-                  <TableCell>{data['pattern']}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TextField
-        onChange={(e) => setBlackListRow({ ...blackListRow, pattern: e.target.value })}
-        value={blackListRow['pattern'] || ''}
-      />
-      <Button variant="contained" onClick={handleSetList}>
-        追加
-      </Button>
+      <BlackListTable />
     </>
   );
 };
