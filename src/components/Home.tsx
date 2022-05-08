5cfee1021401237b1e1adea8642bd9c196eeddff
senkentarou <masahiro-senda@c-fo.com>
2022-05-08 13:43:12 +0900

    :recycle: eslint --fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
-import { AppBar, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography
+} from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const MyAppBar = styled(AppBar)({
@@ -14,25 +25,29 @@ const AppTitle = styled(Typography)({
   marginLeft: '1rem'
 });
 
-type Hash = { [key: string]: string };
+type BlackListStorage = { [key: string]: Array<BlackListRow> };
+type BlackListRow = {
+  pattern?: string;
+  regexp?: boolean;
+};
 
 export const Home: React.VFC = () => {
-  const [blackListTable, setBlackListTable] = useState<Array<Hash>>([]);
-  const [blackListRow, setBlackListRow] = useState<Hash>({});
+  const [blackListTable, setBlackListTable] = useState<Array<BlackListRow>>([]);
+  const [blackListRow, setBlackListRow] = useState<BlackListRow>({});
 
   const handleSetList = () => {
     if (Object.keys(blackListRow).length === 0) {
       return;
     }
     const nextList = [...blackListTable, blackListRow];
-    chrome.storage.local.set({ 'blackListTable': nextList }, () => {
+    chrome.storage.local.set({ blackListTable: nextList }, () => {
       setBlackListTable(nextList);
       setBlackListRow({});
     });
   };
 
   useEffect(() => {
-    chrome.storage.local.get('blackListTable', (data) => {
+    chrome.storage.local.get('blackListTable', (data: BlackListStorage) => {
       setBlackListTable(data['blackListTable'] || []);
     });
   }, []);
@@ -42,33 +57,32 @@ export const Home: React.VFC = () => {
       <MyAppBar position="static">
         <AppTitle variant="h4">Auto Close Tab</AppTitle>
       </MyAppBar>
-      <Typography variant="h5">
-        Blacklisted sites
-      </Typography>
+      <Typography variant="h5">Blacklisted sites</Typography>
       <TableContainer>
         <Table stickyHeader>
           <TableHead>
             <TableRow>
-              <TableCell>
-                pattern
-              </TableCell>
+              <TableCell>pattern</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {blackListTable.map((data) => {
+            {blackListTable.map((data, idx) => {
               return (
-                <TableRow>
-                  <TableCell>
-                    {data['pattern']}
-                  </TableCell>
+                <TableRow key={idx}>
+                  <TableCell>{data['pattern']}</TableCell>
                 </TableRow>
-              )
+              );
             })}
           </TableBody>
         </Table>
       </TableContainer>
-      <TextField onChange={(e) => setBlackListRow({ ...blackListRow, pattern: e.target.value })} value={blackListRow['pattern'] || ''} />
-      <Button variant="contained" onClick={handleSetList}>追加</Button>
+      <TextField
+        onChange={(e) => setBlackListRow({ ...blackListRow, pattern: e.target.value })}
+        value={blackListRow['pattern'] || ''}
+      />
+      <Button variant="contained" onClick={handleSetList}>
+        追加
+      </Button>
     </>
   );
 };
