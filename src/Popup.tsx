73ee61df30550ba9710d82e8d80836f736611114
senkentarou <masahiro-senda@c-fo.com>
2022-05-13 10:43:32 +0900

    add popup mock

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,12 +1,25 @@
 import ReactDOM from 'react-dom';
+import { Button, Container, Switch, Typography } from '@mui/material';
 
-const handleOpenIndex = async () => {
+const handleOpenIndex = () => {
   const url = chrome.runtime.getURL('index.html');
-  await chrome.tabs.create({ url });
+  chrome.tabs.create({ url }).catch((e) => console.error(e));
 };
 
 const Popup = () => {
-  return <button onClick={() => handleOpenIndex}>Configuration details</button>;
+  return (
+    <Container style={{ width: '15rem', height: '13rem' }}>
+      <div style={{ display: 'inline-block' }}>
+        <Typography component="span" style={{ verticalAlign: 'middle', marginRight: '0.5rem' }}>
+          Auto Close Tab
+        </Typography>
+        <Switch />
+      </div>
+      <Button>Add current site to Blacklist</Button>
+      <Button>Add current site to Whitelist</Button>
+      <Button onClick={handleOpenIndex}>Configuration Details</Button>
+    </Container>
+  );
 };
 
 ReactDOM.render(<Popup />, document.getElementById('popup'));
