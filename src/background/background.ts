0d06b5ec61432cd80a40a6445004fd90fedc957d
senkentarou <masahiro-senda@c-fo.com>
2022-05-13 13:34:08 +0900

    fix type error

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,19 +1,35 @@
+type Data = {
+  [key: string]: Array<DataHash>;
+};
+
+type DataHash = {
+  [key: string]: string;
+};
+
 // タブページ更新時
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  chrome.storage.local.get(['blackListTable', 'whiteListTable'], (data) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  chrome.storage.local.get(['blackListTable', 'whiteListTable'], (data: Data) => {
+    // urlが存在しないなら処理しない
     if (!changeInfo.url) {
+      return;
+    }
+
+    // 操作不能を防ぐためchromeのパスは除外する
+    if (changeInfo.url.match(/chrome.*:\/\//)) {
       return;
     }
 
     // ホワイトリストの処理
     if (data['whiteListTable']) {
-      for (const li of data['whiteListTable']) {
-        if (li['regexp']) {
-          const regexp = new RegExp(li['pattern']);
-          if (changeInfo.url?.match(regexp)) {
+      for (const whiteListRow of data['whiteListTable']) {
+        if (whiteListRow['regexp']) {
+          // 正規表現によるパターン一致
+          const regexp = new RegExp(whiteListRow['pattern']);
+          if (changeInfo.url.match(regexp)) {
             return;
           }
-        } else if (changeInfo.url === li['pattern']) {
+        } else if (changeInfo.url === whiteListRow['pattern']) {
+          // 完全一致
           return;
         }
       }
@@ -21,19 +37,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
     // ブラックリストの処理
     if (!data['blackListTable']) {
+      // 登録していないなら処理しない
       return;
     }
 
-    for (const li of data['blackListTable']) {
-      // 正規表現の一致するURLにアクセスしていたらタブを閉じる
-      if (li['regexp']) {
-        const regexp = new RegExp(li['pattern']);
-        if (changeInfo.url?.match(regexp)) {
-          chrome.tabs.remove(tabId);
+    for (const blackListRow of data['blackListTable']) {
+      if (blackListRow['regexp']) {
+        // 正規表現によるパターン一致
+        const regexp = new RegExp(blackListRow['pattern']);
+        if (changeInfo.url.match(regexp)) {
+          // タブを閉じる
+          chrome.tabs.remove(tabId).catch((e: unknown) => console.error(e));
           break;
         }
-      } else if (changeInfo.url === li['pattern']) {
-        chrome.tabs.remove(tabId);
+      } else if (changeInfo.url === blackListRow['pattern']) {
+        // 完全一致
+        chrome.tabs.remove(tabId).catch((e: unknown) => console.error(e));
         break;
       }
     }
@@ -43,6 +62,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 // インストール時
 chrome.runtime.onInstalled.addListener(() => {
   // 設定ページを開く
-  const url = chrome.runtime.getURL("index.html");
-  chrome.tabs.create({ url });
+  const url = chrome.runtime.getURL('index.html');
+  chrome.tabs.create({ url }).catch((e: unknown) => console.error(e));
 });
