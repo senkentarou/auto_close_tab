05b9f4ea1a079141b3dbb398cee574e01e366ab2
senkentarou <masahiro-senda@c-fo.com>
2022-05-13 09:01:40 +0900

    refact for lint error

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom';
 
-const handleOpenIndex = () => {
+const handleOpenIndex = async () => {
   const url = chrome.runtime.getURL('index.html');
-  chrome.tabs.create({ url });
+  await chrome.tabs.create({ url });
 };
 
 const Popup = () => {
-  return <button onClick={handleOpenIndex}>設定</button>;
+  return <button onClick={() => handleOpenIndex}>Configuration details</button>;
 };
 
 ReactDOM.render(<Popup />, document.getElementById('popup'));
